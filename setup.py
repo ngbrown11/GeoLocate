3b38f71c3aa2feadae402b456cfa67b030d9da9a
Nate <ngbrown11@gmail.com>
2017-06-19 00:17:32 -0400

    Add project dependencies

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="GeoLocation",
+    version="1.0",
+    author="Nathaniel Brown"
+    install_requires=[
+        "flask",
+        "jinja2",
+        "werkzeug"
+    ]
+)
